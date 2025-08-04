305c48bb636a5ec6b6cf4b3f264d579cec9f3cca
j05011993 <j05011993@hotmail.com.tw>
2025-08-04 14:33:38 +0800

    repo update

--- src/main/java/com/example/cocktail/repository/CocktailRepository.java
+++ src/main/java/com/example/cocktail/repository/CocktailRepository.java
@@ -3,5 +3,13 @@ package com.example.cocktail.repository;
 import com.example.cocktail.entity.Cocktail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
+
+	Cocktail findByCocktailName(String cocktailName);
+
+	List<Cocktail> findByNameContaining(String input);
+
+	List<Cocktail> getCocktailByBase(Integer baseId);
 }
